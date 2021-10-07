998287f7a0878f5a71537a54916d6a5fdb62e383
victorigoleto <jv.rigoletto@gmail.com>
2021-10-06 22:22:01 -0300

    Projeto msworker criado

---
+++ msworker/src/main/java/com/teste/micro/MsworkerApplication.java
@@ -0,0 +1,13 @@
+package com.teste.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsworkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsworkerApplication.class, args);
+	}
+
+}
---
+++ msworker/src/test/java/com/teste/micro/MsworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
